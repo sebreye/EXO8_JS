444e283038946811a3070d59fe611026158a4d47
sebastian Reyes <sebreyes222gmail.com>
2022-12-20 14:26:36 +0100

    ajout exo1

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+let semaine = prompt("jour de la semaine")
+switch(semaine) {
+    case 'lundi' :
+        alert('tu dois aller à MolenGeek');
+        break;
+    case 'mardi' : 
+        alert('tu as rendez-vous chez le dentiste')
+        break;
+    case 'mercredi' :
+        alert('tu as entrainement de foot')
+    default : 
+        alert("tu n'as rien faire faire frère casse pas la tête")
+}
+let semaine = prompt("jour de la semaine")
+switch(semaine) {
+    case 'lundi' :
+        alert('tu dois aller à MolenGeek');
+        break;
+    case 'mardi' : 
+        alert('tu as rendez-vous chez le dentiste')
+        break;
+    case 'mercredi' :
+        alert('tu as entrainement de foot')
+    default : 
+        alert("tu n'as rien faire faire frère casse pas la tête")
+}
